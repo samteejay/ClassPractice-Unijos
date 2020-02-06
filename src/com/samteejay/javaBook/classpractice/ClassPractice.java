d2c997c8f37e0000646892a4e53cfbd1b70d1147
Samuel Tijani <omalitijani@gmail.com>
2020-02-06 12:58:57 +0100

    Init commit

---
+++ src/com/samteejay/javaBook/classpractice/ClassPractice.java
@@ -0,0 +1,17 @@
+package com.samteejay.javaBook.classpractice;
+
+
+public class ClassPractice {
+    
+    public static void main
+
+    {
+        int a = 5;
+        int b = 8;
+        
+        System.out.println(a++);
+        System.out.println(++b);
+    }
+    
+}
+
